92cab157d273949243ca5f54bc00e6fc1ba02985
catherineliu <catherineliu@orangeblock.tech>
2018-11-09 17:40:10 +0800

    就这样，等待设计图

--- MPChartExample/src/main/java/com/xxmassdeveloper/mpchartexample/BCAASCMultiChartActivity.java
+++ MPChartExample/src/main/java/com/xxmassdeveloper/mpchartexample/BCAASCMultiChartActivity.java
@@ -5,10 +5,13 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.DragEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
 
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.CombinedChart;
 import com.github.mikephil.charting.charts.CombinedChart.DrawOrder;
 import com.github.mikephil.charting.components.Legend;
@@ -22,35 +25,59 @@ import com.github.mikephil.charting.data.CandleData;
 import com.github.mikephil.charting.data.CandleDataSet;
 import com.github.mikephil.charting.data.CandleEntry;
 import com.github.mikephil.charting.data.CombinedData;
+import com.github.mikephil.charting.data.DataSet;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.LargeValueFormatter;
 import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.interfaces.datasets.IDataSet;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.listener.OnDrawListener;
+import com.xxmassdeveloper.mpchartexample.custom.DayAxisValueFormatter;
+import com.xxmassdeveloper.mpchartexample.custom.MyMarkerView;
+import com.xxmassdeveloper.mpchartexample.custom.MyValueFormatter;
+import com.xxmassdeveloper.mpchartexample.custom.XYMarkerView;
 import com.xxmassdeveloper.mpchartexample.notimportant.DemoBase;
 
 import java.util.ArrayList;
 
-public class BCAASCMultiChartActivity extends DemoBase {
+public class BCAASCMultiChartActivity extends DemoBase implements OnChartValueSelectedListener {
 
     private CombinedChart chart;
-    private final int count = 12;
+    private BarChart chartBar;
+    private final int count = 24;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_bcaasc);
+        setContentView(R.layout.activity_bcaasc_mutilple);
 
-        setTitle("BCAASCChartActivity");
+        setTitle("BCAASCMultiChartActivity");
 
         chart = findViewById(R.id.chart1);
+        chartBar = findViewById(R.id.chart_bar);
+
+        initChart();
+        initBarChart2();
+
+    }
+
+
+    private void initChart() {
         chart.getDescription().setEnabled(false);
-        chart.setBackgroundColor(Color.WHITE);
         chart.setDrawGridBackground(false);
         chart.setDrawBarShadow(false);
         chart.setHighlightFullBarEnabled(false);
+//        chart.setOnDragListener(new View.OnDragListener() {
+//            @Override
+//            public boolean onDrag(View v, DragEvent event) {
+//                return false;
+//            }
+//        });
 
         // draw bars behind lines
         chart.setDrawOrder(new DrawOrder[]{
@@ -59,11 +86,10 @@ public class BCAASCMultiChartActivity extends DemoBase {
 
         Legend l = chart.getLegend();
         l.setWordWrapEnabled(true);
-        l.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
-        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
         l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
         l.setDrawInside(false);
-
         YAxis rightAxis = chart.getAxisRight();
         rightAxis.setDrawGridLines(false);
         rightAxis.setAxisMinimum(0f); // this replaces setStartAtZero(true)
@@ -86,14 +112,78 @@ public class BCAASCMultiChartActivity extends DemoBase {
         CombinedData data = new CombinedData();
 
         data.setData(generateLineData());
-        data.setData(generateBarData());
+//        data.setData(generateBarData());
         data.setData(generateCandleData());
         data.setValueTypeface(tfLight);
 
-        xAxis.setAxisMaximum(data.getXMax() + 0.25f);
-
+        xAxis.setAxisMaximum(data.getXMax() + 0.35f);
         chart.setData(data);
         chart.invalidate();
+    }
+
+
+    private void initBarChart2() {
+        chartBar.setOnChartValueSelectedListener(this);
+
+        chartBar.setDrawBarShadow(false);
+        chartBar.setDrawValueAboveBar(true);
+
+        chartBar.getDescription().setEnabled(false);
+
+        // if more than 60 entries are displayed in the chart, no values will be
+        // drawn
+        chartBar.setMaxVisibleValueCount(60);
+
+        // scaling can now only be done on x- and y-axis separately
+        chartBar.setPinchZoom(false);
+
+        chartBar.setDrawGridBackground(false);
+        // chart.setDrawYLabels(false);
+
+        ValueFormatter xAxisFormatter = new DayAxisValueFormatter(chartBar);
+
+        XAxis xAxis = chartBar.getXAxis();
+        xAxis.setPosition(XAxisPosition.BOTTOM);
+        xAxis.setTypeface(tfLight);
+        xAxis.setDrawGridLines(false);
+        xAxis.setGranularity(1f); // only intervals of 1 day
+//        xAxis.setLabelCount(7);
+        xAxis.setValueFormatter(new ValueFormatter() {
+            @Override
+            public String getFormattedValue(float value) {
+                return months[(int) value % months.length];
+            }
+        });
+
+        ValueFormatter custom = new MyValueFormatter("$");
+
+        YAxis leftAxis = chartBar.getAxisLeft();
+        leftAxis.setTypeface(tfLight);
+        leftAxis.setLabelCount(8, false);
+//        leftAxis.setValueFormatter(custom);
+        leftAxis.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART);
+        leftAxis.setSpaceTop(15f);
+        leftAxis.setAxisMinimum(0f); // this replaces setStartAtZero(true)
+
+        YAxis rightAxis = chartBar.getAxisRight();
+        rightAxis.setDrawGridLines(false);
+        rightAxis.setTypeface(tfLight);
+        rightAxis.setLabelCount(8, false);
+//        rightAxis.setValueFormatter(custom);
+        rightAxis.setSpaceTop(15f);
+        rightAxis.setAxisMinimum(0f); // this replaces setStartAtZero(true)
+
+        Legend l = chartBar.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
+        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        l.setDrawInside(false);
+        l.setForm(Legend.LegendForm.SQUARE);
+
+        XYMarkerView mv = new XYMarkerView(this, xAxisFormatter);
+        mv.setChartView(chartBar); // For bounds control
+        chartBar.setMarker(mv); // Set the marker to the chart
+        generateBarData();
     }
 
     private LineData generateLineData() {
@@ -105,60 +195,60 @@ public class BCAASCMultiChartActivity extends DemoBase {
 
         for (int index = 0; index < count; index++) {
             //range是控制曲线度，start是起点
-            float a = getRandom(5, 5);
-            float b = getRandom(4, 5);
-            float c = getRandom(3, 5);
+            float a = getRandom(1, 1);
+            float b = getRandom(2, 1);
+            float c = getRandom(3, 1);
 //            System.out.println("this random is:" + a);
             entries.add(new Entry(index, a));
             entries2.add(new Entry(index, b));
             entries3.add(new Entry(index, c));
         }
         LineDataSet set = new LineDataSet(entries, "Line");
-        set.setColor(Color.rgb(240, 238, 70));
-        set.setLineWidth(2.5f);
-        set.setCircleColor(Color.rgb(240, 238, 70));
-        set.setCircleRadius(3f);
-        set.setFillColor(Color.rgb(240, 238, 70));
+        set.setColor(Color.RED);
+        set.setLineWidth(1.5f);
+        set.setCircleColor(Color.BLACK);
+        set.setCircleRadius(2f);
+        set.setFillColor(Color.BLACK);
         set.setMode(LineDataSet.Mode.CUBIC_BEZIER);
         set.setDrawValues(true);
-        set.setValueTextSize(10f);
-        set.setValueTextColor(Color.rgb(240, 238, 70));
+        set.setValueTextSize(8f);
+        set.setValueTextColor(Color.BLACK);
 
         set.setAxisDependency(YAxis.AxisDependency.LEFT);
 
 
         LineDataSet set2 = new LineDataSet(entries2, "Line2");
-        set2.setColor(Color.rgb(100, 100, 100));
-        set2.setLineWidth(1.0f);
-        set2.setCircleColor(Color.GREEN);
+        set2.setColor(Color.GREEN);
+        set2.setLineWidth(1.5f);
+        set2.setCircleColor(Color.BLACK);
         set2.setCircleRadius(2f);
         set2.setFillColor(Color.GREEN);
         set2.setMode(LineDataSet.Mode.CUBIC_BEZIER);
         set2.setDrawValues(true);
-        set2.setValueTextSize(10f);
-        set2.setValueTextColor(Color.rgb(240, 238, 70));
+        set2.setValueTextSize(8f);
+        set2.setValueTextColor(Color.BLACK);
 
         set2.setAxisDependency(YAxis.AxisDependency.LEFT);
 
 
-        LineDataSet set3 = new LineDataSet(entries3, "Line3");
-        set3.setColor(Color.BLUE);
-        set3.setLineWidth(1.0f);
-        set3.setCircleColor(Color.DKGRAY);
-        set3.setCircleRadius(2f);
-        set3.setMode(LineDataSet.Mode.CUBIC_BEZIER);
-        set3.setDrawCircleHole(false);
-//        set2.setDrawValues(true);
-        set3.setValueTextSize(10f);
-        set3.setValueTextColor(Color.GREEN);
+//        LineDataSet set3 = new LineDataSet(entries3, "Line3");
+//        set3.setColor(Color.BLUE);
+//        set3.setLineWidth(1.0f);
+//        set3.setCircleColor(Color.BLACK);
+//        set3.setCircleRadius(2f);
+//        set3.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+//        set3.setDrawCircleHole(false);
+////        set2.setDrawValues(true);
+//        set3.setValueTextSize(10f);
+//        set3.setValueTextColor(Color.WHITE);
+//
+//        set3.setAxisDependency(YAxis.AxisDependency.LEFT);
 
-        set3.setAxisDependency(YAxis.AxisDependency.LEFT);
-
-        LineData d = new LineData(set, set2, set3);
+        LineData d = new LineData(set, set2);
 
         // create a data object with the data sets
         d.setValueTextColor(Color.BLACK);
-        d.setValueTextSize(11f);
+        d.setValueTextSize(8f);
         return d;
     }
 
@@ -167,45 +257,69 @@ public class BCAASCMultiChartActivity extends DemoBase {
      *
      * @return
      */
-    private BarData generateBarData() {
-
+    private void generateBarData() {
+        BarDataSet set1, set2;
         ArrayList<BarEntry> entries1 = new ArrayList<>();
         ArrayList<BarEntry> entries2 = new ArrayList<>();
 
-        for (int index = 0; index < count; index++) {
-            float a = getRandom(1, 1);
-            float b = getRandom(2, 1);
-            entries1.add(new BarEntry(0, a));
-            entries2.add(new BarEntry(0, b));
-            // stacked
+        if (chartBar.getData() != null && chartBar.getData().getDataSetCount() > 0) {
+            set1 = (BarDataSet) chartBar.getData().getDataSetByIndex(0);
+            set2 = (BarDataSet) chartBar.getData().getDataSetByIndex(1);
+            set1.setValues(entries1);
+            set2.setValues(entries1);
+            chartBar.getData().notifyDataChanged();
+            chartBar.notifyDataSetChanged();
+        } else {
+
+
+            for (int index = 0; index < count; index++) {
+                float a = getRandom(1, 1);
+                float b = getRandom(2, 1);
+                entries1.add(new BarEntry(0, a));
+                entries2.add(new BarEntry(0, b));
+                // stacked
 //            entries2.add(new BarEntry(index, new float[]{getRandom(1, 1), getRandom(1, 1)}));
+            }
+
+            set1 = new BarDataSet(entries1, "Bar 1");
+            set1.setColor(Color.rgb(60, 220, 78));
+            set1.setValueTextColor(Color.rgb(60, 220, 78));
+            set1.setValueTextSize(10f);
+            set1.setAxisDependency(YAxis.AxisDependency.LEFT);
+
+            set2 = new BarDataSet(entries2, "Bar 2");
+//        set2.setStackLabels(new String[]{"Stack 1", "Stack 2"});
+            set2.setColors(Color.rgb(61, 165, 255));
+            set2.setValueTextColor(Color.rgb(61, 165, 255));
+            set2.setValueTextSize(8f);
+            set2.setAxisDependency(YAxis.AxisDependency.LEFT);
+
+
+            BarData data = new BarData(set1, set2);
+            data.setValueFormatter(new LargeValueFormatter());
+            data.setValueTypeface(tfLight);
+
+            chartBar.setData(data);
+
         }
 
-        BarDataSet set1 = new BarDataSet(entries1, "Bar 1");
-        set1.setColor(Color.rgb(60, 220, 78));
-        set1.setValueTextColor(Color.rgb(60, 220, 78));
-        set1.setValueTextSize(10f);
-        set1.setAxisDependency(YAxis.AxisDependency.LEFT);
-
-        BarDataSet set2 = new BarDataSet(entries2, "Bar 2");
-//        set2.setStackLabels(new String[]{"Stack 1", "Stack 2"});
-        set2.setColors(Color.rgb(61, 165, 255), Color.rgb(23, 197, 255));
-        set2.setValueTextColor(Color.rgb(61, 165, 255));
-        set2.setValueTextSize(10f);
-        set2.setAxisDependency(YAxis.AxisDependency.LEFT);
 
         float groupSpace = 0.06f;
         float barSpace = 0.02f; // x2 dataset
         float barWidth = 0.435f; // x2 dataset
         // (0.45 + 0.02) * 2 + 0.06 = 1.00 -> interval per "group"
 
-        BarData d = new BarData(set1, set2);
-        d.setBarWidth(barWidth);
+        // specify the width each bar should have
+        chartBar.getBarData().setBarWidth(barWidth);
 
+        // restrict the x-axis range
+        chartBar.getXAxis().setAxisMinimum(0);
         // make this BarData object grouped
-        d.groupBars(0, groupSpace, barSpace); // start at x = 0
-
-        return d;
+//        data.groupBars(0, groupSpace, barSpace); // start at x = 0
+        // barData.getGroupWith(...) is a helper that calculates the width each group needs based on the provided parameters
+        chartBar.getXAxis().setAxisMaximum(0 + chartBar.getBarData().getGroupWidth(groupSpace, barSpace) * count + 1);
+        chartBar.groupBars(0, groupSpace, barSpace);
+        chartBar.invalidate();
     }
 
 
@@ -222,7 +336,7 @@ public class BCAASCMultiChartActivity extends DemoBase {
 
         for (int index = 0; index < count; index++) {
 
-            float baseValue = (float) (Math.random() * 5) + 10;
+            float baseValue = (float) (Math.random() * 5) + 5;
 
             float open = (float) (Math.random() * 1);//开盘的随机数
             float close = (float) (Math.random() * 1);//收盘的随机数
@@ -257,19 +371,23 @@ public class BCAASCMultiChartActivity extends DemoBase {
 //        set.setBarSpace(150f);
         set.setDrawIcons(false);
         set.setAxisDependency(YAxis.AxisDependency.LEFT);
-        set.setShadowColor(Color.DKGRAY);
-        set.setShadowWidth(0.3f);
+        //设置low or high's color
+//        set.setShadowColor(Color.RED);
+        //设置low or high's color same as candle
+        set.setShadowColorSameAsCandle(true);
+        //设置low or high's stroke's width
+        set.setShadowWidth(1.5f);
         //设置跌的颜色为Red
         set.setDecreasingColor(Color.RED);
         set.setDecreasingPaintStyle(Paint.Style.FILL);
+        set.setNeutralColor(Color.BLUE);
         //设置增长的颜色为绿色
         set.setIncreasingColor(Color.GREEN);
         //设置增长的图形style
         set.setIncreasingPaintStyle(Paint.Style.FILL);
-        set.setNeutralColor(Color.BLUE);
         d.addDataSet(set);
         d.setValueTextColor(Color.BLACK);
-        d.setValueTextSize(11f);
+        d.setValueTextSize(8f);
 
         return d;
     }
@@ -321,4 +439,14 @@ public class BCAASCMultiChartActivity extends DemoBase {
 
     @Override
     public void saveToGallery() { /* Intentionally left empty */ }
+
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
+
+    }
+
+    @Override
+    public void onNothingSelected() {
+
+    }
 }
